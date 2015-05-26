1a8f6b12e44866db5b6bed2c1d6c42517104b7cc
Tracie Wamsley <Tracie Wamsley@10.10.16.94>
2015-05-26 18:03:13 +0000

    Classes create Motor done Time done Can kinda done Robot almost done Sound almost kinda done

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,39 @@
 
-public class Main {
-	public static void main(String[] args) {
+public class Main 
+{
+	public static void main(String[] args) throws InterruptedException 
+	{
+//		BackUpSound vs = new BackUpSound();
+//		vs.play();
+		
+		Time.start();
+		while(Time.calculate() < 15)
+		{
+			System.out.println(Time.calculate());
+			if(Time.calculate() == 10)
+			{
+				Time.timeToClear = Time.calculate();
+			}
+			Thread.sleep(1000);
+		}
+		System.out.println("Time: " + Time.timeToClear);
+		Thread.sleep(3000);
+			
+		
+		//Find can in circle
+//		Can can = new Can();
+//		can.findCanInCircle();
+		/*
+		 *  Find can in circle
+			While cans exist … push can out of circle
+			Play appropriate sound 
+			Calculate time
+			Display appropriate message
+			leave circle
+		 */
+		
 		System.out.println("FASDF");
 	}
+	
+	
 }
